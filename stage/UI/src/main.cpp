33da3ad2d82c9de08a0212e76c3f52c452f986a1
mariyaaaach <116644691+mariyaaach@users.noreply.github.com>
2024-04-15 20:18:59 +0300

    add fonts

--- stage/UI/src/main.cpp
+++ stage/UI/src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/mainwindow.h"
 #include <QFile>
-
+#include <QFont>
+#include <QFontDatabase>
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -12,6 +13,28 @@ int main(int argc, char *argv[])
     a.setStyleSheet(file.readAll());
     MainWindow w;
     w.show();
+
+
+    int boldFontId = QFontDatabase::addApplicationFont(":/fonts/RG-StandardBold.ttf");
+    if (boldFontId == -1) {
+        qDebug() << "Failed to load RG-StandardBold font!";
+    } else {
+        QString boldFamily = QFontDatabase::applicationFontFamilies(boldFontId).at(0);
+        // Можете установить этот шрифт как шрифт приложения, если хотите
+        // QFont boldFont(boldFamily);
+        // QApplication::setFont(boldFont);
+    }
+
+    int bookFontId = QFontDatabase::addApplicationFont(":/fonts/RG-StandardBook.ttf");
+    if (bookFontId == -1) {
+        qDebug() << "Failed to load RG-StandardBook font!";
+    } else {
+        QString bookFamily = QFontDatabase::applicationFontFamilies(bookFontId).at(0);
+        // Можете установить этот шрифт как шрифт приложения, если хотите
+        // QFont bookFont(bookFamily);
+        // QApplication::setFont(bookFont);
+    }
+
 
     return a.exec();
 }
