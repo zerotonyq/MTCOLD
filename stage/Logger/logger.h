148f50166be8e49bdb5c8fac7a182e0eb4b113d1
IlyaL2004 <129002007+IlyaL2004@users.noreply.github.com>
2024-06-08 19:08:29 +0300

    Update logger.h

--- stage/Logger/logger.h
+++ stage/Logger/logger.h
@@ -10,37 +10,51 @@
 
 namespace Logs {
 
-class Output_to_file
+class Output_location
+{
+public:
+    virtual ~Output_location() = default;
+    virtual void write(const std::string& data) = 0;
+};
+
+class Output_to_file : public Output_location
 {
 public:
     Output_to_file(const std::string& file_name);
-    void write(const std::string& data);
+    void write(const std::string& data) override;
 
 private:
     std::ofstream m_output;
     std::mutex m_mutex;
 };
 
-class Output_to_console
+class Output_to_console : public Output_location
 {
 public:
-    void write(const std::string& data);
+    void write(const std::string& data) override;
 };
-
 
 class Logger
 {
 public:
+    enum class Log_output
+    {
+        File_and_console_case,
+        File_only_case,
+        Console_only_case
+    };
 
-    Logger(const std::string& file_name, bool file, bool cons);
+    Logger(const std::string& file_name, Log_output output);
     void log(std::string level, const std::string& source, const std::string& message);
     void log_packet_in_log(std::string level, const std::string& source, MainPacket packet);
 
 private:
-    Output_to_console m_output_to_console;
-    Output_to_file m_output_to_file;
-    bool m_write_to_file;
-    bool m_write_to_console;
+    void write_log(const std::string& formatted_message);
+    std::string format_message(const std::string& level, const std::string& source, const std::string& message);
+
+    Output_to_console m_consoleSink;
+    Output_to_file m_fileSink;
+    Log_output m_output;
 };
 
 }
@@ -55,6 +69,9 @@ private:
 #define LOG_ERROR(message) logger.log("ERROR", __FILE__ ":" STR(__LINE__), (message))
 #define LOG_FATAL(message) logger.log("FATAL", __FILE__ ":" STR(__LINE__), (message))
 #define LOG_PACKET(pac) logger.log_packet_in_log("PACKET", __FILE__ ":" STR(__LINE__), (pac))
-#define logger(file_name, file, consol)  Logs::Logger logger(file_name, file, consol)
+#define File_and_console Logs::Logger::Log_output::File_and_console_case
+#define Console_only Logs::Logger::Log_output::Console_only_case
+#define File_only Logs::Logger::Log_output::File_only_case
+#define logger(file_name, output)  Logs::Logger logger(file_name, output)
 
 #endif
