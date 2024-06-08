40847b5228b4654c84ab59457bd18e60b79d81b6
Vika <viktoriya.sokolova.04@gmail.com>
2024-06-08 17:56:14 +0300

    end3!

--- stage/UI/src/mainwindow.cpp
+++ stage/UI/src/mainwindow.cpp
@@ -10,17 +10,24 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // this->setWindowFlags(Qt::CustomizeWindowHint); отсутсвие рамки
-
 
     ui ->api ->setPlaceholderText("Введите IP-адрес");
     ui ->port ->setPlaceholderText("Введите порт");
     // connect(core, &Core::getIndicatorsQuantityPacket, this, [=](const sIndicatorsCountPack& indicatorsCountPack) {
-    //     QWidget::close();
-    //     indicator indicator(core);
-    //     indicator.setModal(true);
-    //     indicator.currentIndicatorsQuantity = indicatorsCountPack.indicatorsCount;
-    //     indicator.exec();
+    //     QHostAddress hostAddress(api);
+    //     if (!(hostAddress.isNull() || hostAddress.protocol() == QAbstractSocket::UnknownNetworkLayerProtocol) && port.toUShort() >= 1 && port.toUShort() <= 65535) {
+    //         core = new Core(api,port.toUShort());
+    //         indicator indicator(core);
+    //         indicator.currentIndicatorsQuantity = indicatorsCountPack.indicatorsCount;
+    //         QWidget::close();
+    //         indicator.setModal(true);
+    //         indicator.exec();
+    //     }
+    //     else {
+    //         QString message = "<html><style>p {color: white;}</style><p>Неверный API или порт</p></body></html>";
+    //         QMessageBox msgBox;
+    //         msgBox.warning(this, "Error", message);
+    //     }
     // });
 
 }
@@ -30,38 +37,13 @@ MainWindow::~MainWindow(){
 }
 
 void MainWindow::on_request_clicked() {
-    QString api = ui -> api -> text();
-    QString port = ui -> port -> text();
+    api = ui -> api -> text();
+    port = ui -> port -> text();
 
-    quint16 int_port = port.toUShort();
-
-    //Core core(api,int_port);
-    //restart.setCore(&core);
     //core->getIndicatorsQuantity();
     QWidget::close();
     indicator indicator(core);
     indicator.setModal(true);
     indicator.currentIndicatorsQuantity = 4;
     indicator.exec();
-
-
-
-    // QHostAddress hostAddress(api);
-    // if (!(hostAddress.isNull() || hostAddress.protocol() == QAbstractSocket::UnknownNetworkLayerProtocol) && int_port >= 1 && int_port <= 65535) {
-    //     core = new Core(api,int_port);
-    //     restart* Restart = new restart();
-    //     Restart->setCore(core);
-    //     QWidget::close();
-    //     indicator.setModal(true);
-    //     indicator.exec();
-    // }
-    // else {
-    //     QString message = "<html><style>p {color: white;}</style><p>Неверный API или порт</p></body></html>";
-    //     QMessageBox msgBox;
-    //     msgBox.warning(this, "Error", message);
-    //     // QString message = "<html><p style=;color:red;"">Неверный API или порт</p></html>";
-
-    //     // QLabel *label = new QLabel(message);
-    //     // label->setAlignment(Qt::AlignCenter);
-    //     // label->setStyleSheet("QLabel { color : red; }");
 }
